31bff3ac43884f1bb49c45113662868b7e0deed1
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-11-28 14:29:26 +0100

    fix requirement error 3

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
-# create a file in /tmp
-file {'school':
+#Create a file in /tmp
+
+file { 'school':
 ensure  => 'present',
-path    => 'tmp/school',
+path    => '/tmp/school',
+mode    => '0744',
 owner   => 'www-data',
 group   => 'www-data',
-mode    => '0744',
 content => 'I love Puppet'
 }
