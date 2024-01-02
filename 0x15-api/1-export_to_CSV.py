6ba103ca9d7215fdec91fe41d47fe7125358d8b3
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-01-02 22:35:29 +0100

    task 2: fix 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,16 +12,16 @@ if __name__ == '__main__':
     if len(argv) > 1:
         if re.fullmatch(r'\d+', argv[1]):
             API_URL = 'https://jsonplaceholder.typicode.com/'
-        id = int(argv[1])
-        user_res = requests.get('{}/users/{}'.format(API_URL, id)).json()
-        todos_res = requests.get('{}/todos'.format(API_URL)).json()
-        user_name = user_res.get('username')
-        todos = list(filter(lambda x: x.get('userId') == id, todos_res))
-        with open('{}.csv'.format(id), 'w') as file:
-            for todo in todos:
-                file.write('"{}","{}","{}","{}"\n'.format(
-                    id,
-                    user_name,
-                    todo['completed'],
-                    todo['title']
-                ))
+            id = int(argv[1])
+            user_res = requests.get('{}/users/{}'.format(API_URL, id)).json()
+            todos_res = requests.get('{}/todos'.format(API_URL)).json()
+            user_name = user_res.get('username')
+            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
+            with open('{}.csv'.format(id), 'w') as file:
+                for todo in todos:
+                    file.write('"{}","{}","{}","{}"\n'.format(
+                        id,
+                        user_name,
+                        todo['completed'],
+                        todo['title']
+                    ))
