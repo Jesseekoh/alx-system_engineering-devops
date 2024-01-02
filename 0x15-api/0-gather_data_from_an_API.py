c23c27e6fa40421312bc7fb36c63a3e6e9190f9b
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-01-02 20:59:59 +0100

    task 0: add file

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import re
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    if len(argv) > 1:
+        if re.fullmatch(r'\d+', argv[1]):
+            API_URL = 'https://jsonplaceholder.typicode.com/'
+        user_id = int(argv[1])
+        user_res = requests.get('{}/users/{}'.format(API_URL, user_id)).json()
+        todos_res = requests.get('{}/todos'.format(API_URL)).json()
+        user_name = user_res.get('name')
+        todos = list(filter(lambda x: x.get('userId') == id, todos_res))
+        todos_done = list(filter(lambda x: x.get('completed'), todos))
+        print(
+            'Employee {} is done with tasks({}/{}):'.format(
+                user_name,
+                len(todos_done),
+                len(todos)
+            )
+        )
+        for todo_done in todos_done:
+            print('\t {}'.format(todo_done.get('title')))
