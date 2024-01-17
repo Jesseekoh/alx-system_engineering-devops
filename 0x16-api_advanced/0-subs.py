92ccd059d4964ccf9593caba6baa54705ca99476
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-01-17 19:59:43 +0100

    task 0: add file

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""0-0-subs.py module"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Gets the number of subscribers a subreddit has"""
+    BASE_URL = 'https://www.reddit.com'
+
+    api_headers = {
+        'Accept': 'application/json'
+    }
+    res = requests.get(
+      '{}/r/{}/about/.json'.format(BASE_URL, subreddit),
+      allow_redirects=False,
+      headers=api_headers
+    )
+
+    if res.status_code == 200:
+        data = res.json()
+        return data['data']['subscribers']
+    return 0
