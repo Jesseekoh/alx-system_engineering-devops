9c2a84a5be4fb4cd0ebab8e89934c9532c410e46
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-01-19 08:53:08 +0100

    task 0: fix 2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,20 +7,11 @@ def number_of_subscribers(subreddit):
     """Gets the number of subscribers a subreddit has"""
     BASE_URL = 'https://www.reddit.com'
 
-    api_headers = {
-        'Accept': 'application/json',
-        'User-Agent': ' '.join([
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
-            'AppleWebKit/537.36 (KHTML, like Gecko)',
-            'Chrome/97.0.4692.71',
-            'Safari/537.36',
-            'Edg/97.0.1072.62'
-        ])
-    }
+    headers = {'User-Agent': 'My User Agent 1.0'}
     res = requests.get(
         '{}/r/{}/about/.json'.format(BASE_URL, subreddit),
         allow_redirects=False,
-        headers=api_headers
+        headers=headers
     )
 
     if res.status_code == 200:
