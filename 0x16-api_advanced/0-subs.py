c2e64ffa0afc4477528f258d43759239411c5e09
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-01-28 08:06:55 +0100

    task 0: fix 2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
-"""0-subs.py module"""
+'''A module containing functions for working with the Reddit API.
+'''
 import requests
 
 
-def number_of_subscribers(subreddit):
-    """Gets the number of subscribers a subreddit has"""
-    BASE_URL = 'https://www.reddit.com'
+BASE_URL = 'https://www.reddit.com'
+'''Reddit's base API URL.
+'''
 
-    headers = {'User-Agent': 'My User Agent 1.0'}
+
+def number_of_subscribers(subreddit):
+    '''Retrieves the number of subscribers in a given subreddit.
+    '''
+    api_headers = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
+    }
     res = requests.get(
         '{}/r/{}/about/.json'.format(BASE_URL, subreddit),
-        allow_redirects=False,
-        headers=headers
+        headers=api_headers,
+        allow_redirects=False
     )
-
     if res.status_code == 200:
-        data = res.json()
-        return data['data']['subscribers']
+        return res.json()['data']['subscribers']
     return 0
