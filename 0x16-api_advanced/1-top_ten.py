b806dfe9ba0f7052af9c8d6f13a1a470fff2acd8
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-01-28 08:05:26 +0100

    task 1: fix 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
-"""1-1-top_ten module"""
+'''A module containing functions for working with the Reddit API.
+'''
 import requests
 
 
-def top_ten(subreddit):
-    """
-    Prints the titles of the first 10 hot posts listed for a given
-    subreddit
-    """
-    BASE_URL = 'https://www.reddit.com'
+BASE_URL = 'https://www.reddit.com'
+'''Reddit's base API URL.
+'''
 
+
+def top_ten(subreddit):
+    '''Retrieves the title of the top ten posts from a given subreddit.
+    '''
     api_headers = {
         'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
     }
     sort = 'top'
     limit = 10
