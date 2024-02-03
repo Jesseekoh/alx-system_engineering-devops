9a5aa8a82d37ec3d5e9741ea5eeda3c59dd0dd32
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-02-04 00:29:17 +0100

    task 0: fix 3

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
-'''A module containing functions for working with the Reddit API.
-'''
-import requests
-
-
-BASE_URL = 'https://www.reddit.com'
-'''Reddit's base API URL.
-'''
 
 
 def number_of_subscribers(subreddit):
-    '''Retrieves the number of subscribers in a given subreddit.
-    '''
-    api_headers = {
-        'Accept': 'application/json',
-        'User-Agent': ' '.join([
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
-            'AppleWebKit/537.36 (KHTML, like Gecko)',
-            'Chrome/97.0.4692.71',
-            'Safari/537.36',
-            'Edg/97.0.1072.62'
-        ])
-    }
-    res = requests.get(
-        '{}/r/{}/about/.json'.format(BASE_URL, subreddit),
-        headers=api_headers,
-        allow_redirects=False
-    )
-    if res.status_code == 200:
-        return res.json()['data']['subscribers']
+    """ Returns subscriber count of subreddit or 0 """
+    from requests import get
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    headers = {'user-agent': 'my-app/0.0.1'}
+
+    r = get(url, headers=headers, allow_redirects=False)
+
+    if r.status_code != 200:
+        return 0
+
+    try:
+        js = r.json()
+
+    except ValueError:
+        return 0
+
+    data = js.get("data")
+
+    if data:
+        sub_count = data.get("subscribers")
+        if sub_count:
+            return sub_count
+
     return 0
